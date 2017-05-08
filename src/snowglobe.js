e19bc883f869d00324b39fdd3e893861085f392d
Mahmoud Swehli <m_swehli@hotmail.com>
2017-05-08 21:38:07 +0200

    small updates

---
+++ src/snowglobe.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var moodio = moodio || {};
+moodio.snowglobe = moodio.snowglobe || {};
+
+
+(function(sg){
+    function sgCreate(elem)
+    {
+        //get options
+
+        //view port details in units
+        this.viewport = {};
+        this.viewport.width = 0;
+        this.viewport.height = 0;
+        this.viewport.viewerDistance = 200;
+    }
+
+})(moodio.snowglobe);
+
+//run init on load completion
